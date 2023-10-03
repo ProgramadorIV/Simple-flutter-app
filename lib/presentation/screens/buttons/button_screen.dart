dedcefc2b194554030494c2189a92ce842fefdb8
Your Name <antoniojz1998@gmail.com>
2023-10-04 01:36:15 +0200

    Custom button

--- lib/presentation/screens/buttons/button_screen.dart
+++ lib/presentation/screens/buttons/button_screen.dart
@@ -73,7 +73,34 @@ class _ButtonsView extends StatelessWidget {
                 //All is used for all the states of the button -> ex. splash
               ),
             ),
+            const _CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  const _CustomButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Text(
+              'The last button',
+              style: TextStyle(color: Colors.amber),
+            ),
+          ),
         ),
       ),
     );
