f0c37570f72dac01c01f05314233d8af4dc28d93
Your Name <antoniojz1998@gmail.com>
2023-10-03 14:26:53 +0200

    Implemented home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_flutter_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,49 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Simple App'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(item.title),
+      trailing: Icon(
+        Icons.arrow_forward_outlined,
+        color: colors.primary,
+      ),
+      subtitle: Text(item.subTitle),
+      leading: Icon(
+        item.icon,
+        color: colors.primary,
+      ),
+      onTap: () {},
     );
   }
 }
