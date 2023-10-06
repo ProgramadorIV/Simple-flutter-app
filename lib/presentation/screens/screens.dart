6e95df477ac6eca9aabef2ef4bad5836e1ce61fb
Your Name <antoniojz1998@gmail.com>
2023-10-06 02:20:32 +0200

    All other screens

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'animated/animated_screen.dart';
+export 'app_tutorial/app_tutorial_screen.dart';
 export 'buttons/button_screen.dart';
 export 'cards/cards_screen.dart';
 export 'home/home_screen.dart';
+export 'infinite_scroll/infinite_scroll.dart';
+export 'progress/progress_screen.dart';
+export 'snackbar/snackbar.dart';
+export 'ui_controls/ui_controls.dart';
---
+++ lib/presentation/screens/ui_controls/ui_controls.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UIControlScreen extends StatelessWidget {
+  const UIControlScreen({super.key});
+
+  static const name = 'ui_controls_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
