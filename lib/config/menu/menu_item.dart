b7b5038b262bcd7d83503c3e841e27a3f3e2bb48
Your Name <antoniojz1998@gmail.com>
2023-10-06 13:30:15 +0200

    Dialog and snackbar example

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -28,9 +28,15 @@ const appMenuItems = <MenuItem>[
     icon: Icons.credit_card,
   ),
   MenuItem(
-    title: 'ProgressIndicators',
+    title: 'Progress Indicators',
     subTitle: 'Lazy screen to show progress',
     link: '/progress',
     icon: Icons.refresh_outlined,
+  ),
+  MenuItem(
+    title: 'Snackbars and dialogs',
+    subTitle: 'Info widgets',
+    link: '/snackbars',
+    icon: Icons.info_outline,
   ),
 ];
--- lib/presentation/screens/snackbar/snackbar.dart
+++ lib/presentation/screens/snackbar/snackbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   const SnackbarScreen({super.key});
@@ -7,6 +8,75 @@ class SnackbarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbar and dialogs'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackbar(context),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        label: const Text('Show snackbar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text(
+                        'The shown licenses are used by this flutter project. Please click on \'View licenses\' to take a look.')
+                  ],
+                );
+              },
+              child: const Text('Software licenses'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Show dialog'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Hello, snackbar here'),
+        action: SnackBarAction(label: '', onPressed: () {}),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false, //Obligamos a pulsar alguno de los dos botones
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure about that?'),
+        // content: Image.network(
+        //   'https://media.tenor.com/XQ0mT-V1n30AAAAC/are-you-sure-about-that-the-rock.gif',
+        //   errorBuilder: (context, error, stackTrace) =>
+        //       const Text('Ups, no meme'),
+        // ),
+        content: const Text('There is no coming back from this!'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancel'),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text('Accept'),
+          ),
+        ],
+      ),
+    );
   }
 }
