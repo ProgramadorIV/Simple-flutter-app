d60cfbb8c095b5c1fe6b81edd7cc1f845e9eb225
Your Name <antoniojz1998@gmail.com>
2023-10-10 01:35:09 +0200

    change whole app themes

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -11,14 +11,16 @@ const colorList = <Color>[
 
 class AppTheme {
   final int selectedColor;
+  final bool isDark;
 
-  AppTheme({this.selectedColor = 0})
+  AppTheme({this.selectedColor = 0, this.isDark = false})
       : assert(selectedColor >= 0, 'Color selected must be over 0'),
         assert(selectedColor < colorList.length,
             'Selected color can not exceed ${colorList.length - 1}');
 
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
+      brightness: isDark ? Brightness.dark : Brightness.light,
       colorSchemeSeed: colorList[selectedColor],
       appBarTheme: const AppBarTheme(centerTitle: false));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_flutter_app/config/routing/app_router.dart';
 import 'package:simple_flutter_app/config/theme/app_theme.dart';
+import 'package:simple_flutter_app/presentation/providers/theme_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     return MaterialApp.router(
       title: 'Simple App',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 0).getTheme(),
+      theme: AppTheme(
+        selectedColor: ref.watch(selectedColorProvider),
+        isDark: ref.watch(isDarkProvider),
+      ).getTheme(),
     );
   }
 }
